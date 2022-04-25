6b12033dc9eb49bb3682f064f82dc4d51a150d00
nhokkoranh1511 <nhokkoranh1511@gmail.com>
2022-04-25 15:59:23 +0700

    update admin

---
+++ source_code/admin/user-management.php
@@ -0,0 +1,46 @@
+<?php 
+    include('partial/partial.php')
+?>
+
+<?php addHeader("Quản lý người dùng"); ?>
+
+<div class="container">
+    <h1>
+        Quản lý người dùng
+</div>
+
+
+<div class="container">
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td colspan="2">Larry the Bird</td>
+                <td>@twitter</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+
+<?php addFooter(); ?>
