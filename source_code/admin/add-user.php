0883b5978d23f2b444eac50b591806555a5ec1b2
nhokkoranh1511 <nhokkoranh1511@gmail.com>
2022-04-26 17:05:41 +0700

    Update admin

--- source_code/admin/add-user.php
+++ source_code/admin/add-user.php
@@ -2,37 +2,71 @@
     include('partial/partial.php');
     $path = $_SERVER['DOCUMENT_ROOT'];
     include_once($path."/handler/includeHandler.php");
+    include_once($path."/admin/macro/checkInput.php");
+    
+    if ($loginHandler->checkLogin() == false) {
+        redirect("/index.php");
+    }
+    else { // logged in
+        $id_admin = $userHandler->loggedUser['id'];
+    }
+    
+    $errorUsername  = "";
+    $errorPassword  = "";
+    $errorFull_name = "";
+    $errorEmail     = "";
 ?>
 
 <?php addHeader("Thêm tài khoản"); ?>
 
-<div class="text-center container">
+<div class="container">
+
+</div>
+
+<?php
+    if (isset($_POST['submit'])) {
+        $errorUsername      = checkString($_POST['username']);
+        $errorPassword      = checkString($_POST['password']);
+        $errorFull_name     = checkString($_POST['full_name']);
+        $errorEmail         = checkNull($_POST['mail']);
+
+        if ($errorUsername == "" && $errorPassword == "" && $errorFull_name == "" && $errorEmail == "") {
+            $user = new UserDB();
+            $user->insert_user(cleanInput($_POST['username']), cleanInput($_POST['password']),
+            cleanInput($_POST['full_name']), cleanInput($_POST['privil']), cleanInput($_POST['mail']));
+            redirect("/admin/user-management.php");
+        }        
+    }
+?>
+
+<div class="container">
     <div class="row">
         <div class="col-sm-3"></div>
         <div class="col-sm-6">
             <form method="POST" action="">
                 <div class="form-floating">
                     <input type="text" class="form-control" name="username" placeholder="username">
-                    <label for="username">Tài khoản <span class="error">*</span></label>
+                    <label for="username">Tài khoản <span class="error">* <?php echo $errorUsername;?></span></label>
                 </div>
                 <div class="form-floating">
                     <input type="password" class="form-control" name="password" placeholder="Password">
-                    <label for="password">Mật khẩu <span class="error">*</span></label>
+                    <label for="password">Mật khẩu <span class="error">* <?php echo $errorPassword;?></span></label>
                 </div>
                 <div class="form-floating">
                     <input type="text" class="form-control" name="full_name" placeholder="full_name">
-                    <label for="password">Họ và tên <span class="error">*</span></label>
+                    <label for="password">Họ và tên <span class="error">* <?php echo $errorFull_name;?></span></label>
                 </div>
+                <div class="form-floating">
+                    <input type="email" class="form-control" name="mail" placeholder="mail">
+                    <label for="password">Email <span class="error">* <?php echo $errorEmail;?></span></label>
+                </div>
+                <br>
+                <label for="privil">Quyền </label>
                 <select class="form-select" aria-label="Default select example" name="privil">
-                    <option selected>Quyền</option>
-                    <option value="admin">admin</option>
+                    <option selected>admin</option>
                     <option value="staff">staff</option>
                     <option value="user">user</option>
                 </select>
-                <div class="form-floating">
-                    <input type="email" class="form-control" name="mail" placeholder="mail">
-                    <label for="password">Email <span class="error">*</span></label>
-                </div>
                 <div class="checkbox mb-3">
                 </div>
                 <input class="w-100 btn btn-lg btn-dark" type="submit" name="submit" value="Thêm người dùng"></input>
@@ -44,12 +78,3 @@
 </div>
 
 <?php addFooter(); ?>
-
-<?php
-    if (isset($_POST['submit'])) {
-        $user = new UserDB();
-        $user->insert_user($_POST['username'],$_POST['password'],$_POST['full_name'],$_POST['privil'],$_POST['mail']);
-    
-        redirect("/admin/user-management.php");
-    }
-?>
--- source_code/admin/macro/updateUser.php
+++ source_code/admin/macro/updateUser.php
@@ -1,7 +1,15 @@
 <?php
-    $path = $_SERVER['DOCUMENT_ROOT'];
-    include_once($path . '/admin/partial/partial.php');
-    include_once($path . "/macro/includemacro.php");
+$path = $_SERVER['DOCUMENT_ROOT'];
+include_once($path . '/admin/partial/partial.php');
+include_once($path . "/macro/includemacro.php");
+include_once($path . "/admin/macro/checkInput.php");
+include_once($path . "/handler/includeHandler.php");
+
+if ($loginHandler->checkLogin() == false) {
+    redirect("/index.php");
+} else { // logged in
+    $id_admin = $userHandler->loggedUser['id'];
+}
 ?>
 
 <?php
@@ -21,37 +29,88 @@ $email          = $row['email'];
         $("#button1").click(function() {
             var full_name = $("#full_name").val();
 
-            $.ajax({
-                method: "POST",
-                url: "update/updateFullname.php?id=<?php echo $id; ?>",
-                data: {full_name: full_name}
-            });
+            var error = "";
 
-            document.getElementById("old_full_name").innerHTML = full_name;
+            if (!full_name) {
+                error = "* Xin hãy nhập thông tin!";
+            } else {
+                if (full_name.length < 4 || full_name.length > 40) {
+                    error = "* Giá trị không hợp lệ!";
+                }
+            }
+
+            if (error == "") {
+                $.ajax({
+                    method: "POST",
+                    url: "update/updateFullname.php?id=<?php echo $id; ?>",
+                    data: {
+                        full_name: full_name
+                    }
+                });
+
+                document.getElementById("old_full_name").innerHTML = full_name;
+            }
+            else {
+                document.getElementById("errorFull_name").innerHTML = error;
+            }
         });
 
         $("#button2").click(function() {
             var username = $("#username").val();
 
-            $.ajax({
-                method: "POST",
-                url: "update/updateUsername.php?id=<?php echo $id; ?>",
-                data: {username: username}
-            });
+            var error = "";
 
-            document.getElementById("old_username").innerHTML = username;
+            if (!username) {
+                error = "* Xin hãy nhập thông tin!";
+            } else {
+                if (username.length < 4 || username.length > 40) {
+                    error = "* Giá trị không hợp lệ!";
+                }
+            }
+
+            if (error == "") {
+                $.ajax({
+                    method: "POST",
+                    url: "update/updateUsername.php?id=<?php echo $id; ?>",
+                    data: {
+                        username: username
+                    }
+                });
+
+                document.getElementById("old_username").innerHTML = username;
+            }
+            else {
+                document.getElementById("errorUsername").innerHTML = error;
+            }
         });
 
         $("#button3").click(function() {
             var password = $("#password").val();
 
-            $.ajax({
-                method: "POST",
-                url: "update/updatePassword.php?id=<?php echo $id; ?>",
-                data: {password: password}
-            });
+            var error = "";
 
-            document.getElementById("old_password").innerHTML = password;
+            if (!password) {
+                error = "* Xin hãy nhập thông tin!";
+            } else {
+                if (password.length < 4 || password.length > 40) {
+                    error = "* Giá trị không hợp lệ!";
+                }
+            }
+
+            if (error == "") {
+                $.ajax({
+                    method: "POST",
+                    url: "update/updatePassword.php?id=<?php echo $id; ?>",
+                    data: {
+                        password: password
+                    }
+                });
+
+                document.getElementById("old_password").innerHTML = password;
+            }
+            else {
+                document.getElementById("errorPassword").innerHTML = error;
+            }
         });
 
         $("#button4").click(function() {
@@ -60,7 +119,9 @@ $email          = $row['email'];
             $.ajax({
                 method: "POST",
                 url: "update/updatePrivil.php?id=<?php echo $id; ?>",
-                data: {privil: privil}
+                data: {
+                    privil: privil
+                }
             });
 
             document.getElementById("old_privil").innerHTML = privil;
@@ -69,18 +130,35 @@ $email          = $row['email'];
         $("#button5").click(function() {
             var email = $("#mail").val();
 
-            $.ajax({
-                method: "POST",
-                url: "update/updateEmail.php?id=<?php echo $id; ?>",
-                data: {email: email}
-            });
+            var error = "";
 
-            document.getElementById("old_email").innerHTML = email;
+            if (!email) {
+                error = "* Xin hãy nhập thông tin!";
+            } else {
+                if (email.length < 4 || email.length > 40) {
+                    error = "* Giá trị không hợp lệ!";
+                }
+            }
+
+            if (error == "") {
+                $.ajax({
+                    method: "POST",
+                    url: "update/updateEmail.php?id=<?php echo $id; ?>",
+                    data: {
+                        email: email
+                    }
+                });
+
+                document.getElementById("old_email").innerHTML = email;
+            }
+            else {
+                document.getElementById("errorMail").innerHTML = error;
+            }
         });
     });
 </script>
 
-<?php addHeader("Cập nhật tài khoản"); ?>
+<?php addHeader("Chỉnh sửa"); ?>
 
 <form class="container">
     <table class="table">
@@ -94,29 +172,12 @@ $email          = $row['email'];
         </thead>
         <tbody>
             <tr>
-                <th scope="row">Họ và tên</th>
-                <td id="old_full_name"><?php echo $full_name; ?></td>
-                <td>
-                    <div class="form-floating">
-                        <input type="text" class="form-control" id="full_name" placeholder="full_name">
-                        <label for="password">Họ và tên <span class="error">*</span></label>
-                    </div>
-                </td>
-                <td>
-                    <div class="d-grid gap-2" type="submit">
-                        <a id="button1" class="btn btn-secondary">
-                            Chỉnh sửa
-                        </a>
-                    </div>
-                </td>
-            </tr>
-            <tr>
                 <th scope="row">username</th>
                 <td id="old_username"><?php echo $username; ?></td>
                 <td>
                     <div class="form-floating">
                         <input type="text" class="form-control" id="username" placeholder="username">
-                        <label for="username">Tài khoản <span class="error">*</span></label>
+                        <label for="username">Tài khoản <span class="error" id="errorUsername">*</span></label>
                     </div>
                 </td>
                 <td>
@@ -132,7 +193,7 @@ $email          = $row['email'];
                 <td>
                     <div class="form-floating">
                         <input type="password" class="form-control" id="password" placeholder="Password">
-                        <label for="password">Mật khẩu <span class="error">*</span></label>
+                        <label for="password">Mật khẩu <span class="error" id="errorPassword">*</span></label>
                     </div>
                 </td>
                 <td>
@@ -144,12 +205,45 @@ $email          = $row['email'];
                 </td>
             </tr>
             <tr>
+                <th scope="row">Họ và tên</th>
+                <td id="old_full_name"><?php echo $full_name; ?></td>
+                <td>
+                    <div class="form-floating">
+                        <input type="text" class="form-control" id="full_name" placeholder="full_name">
+                        <label for="password">Họ và tên <span class="error" id="errorFull_name">*</span></label>
+                    </div>
+                </td>
+                <td>
+                    <div class="d-grid gap-2" type="submit">
+                        <a id="button1" class="btn btn-secondary">
+                            Chỉnh sửa
+                        </a>
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <th scope="row">email</th>
+                <td id="old_email"><?php echo $email; ?></td>
+                <td>
+                    <div class="form-floating">
+                        <input type="email" class="form-control" id="mail" placeholder="mail">
+                        <label for="password">Email <span class="error" id="errorMail">*</span></label>
+                    </div>
+                </td>
+                <td>
+                    <div class="d-grid gap-2" type="submit">
+                        <a id="button5" class="btn btn-secondary">
+                            Chỉnh sửa
+                        </a>
+                    </div>
+                </td>
+            </tr>
+            <tr>
                 <th scope="row">Quyền</th>
                 <td id="old_privil"><?php echo $privil; ?></td>
                 <td>
                     <select class="form-select" aria-label="Default select example" id="privil">
-                        <option selected>Quyền</option>
-                        <option value="admin">admin</option>
+                        <option selected>admin</option>
                         <option value="staff">staff</option>
                         <option value="user">user</option>
                     </select>
@@ -163,20 +257,8 @@ $email          = $row['email'];
                 </td>
             </tr>
             <tr>
-                <th scope="row">email</th>
-                <td id="old_email"><?php echo $email; ?></td>
-                <td>
-                    <div class="form-floating">
-                        <input type="email" class="form-control" id="mail" placeholder="mail">
-                        <label for="password">Email <span class="error">*</span></label>
-                    </div>
-                </td>
-                <td>
-                    <div class="d-grid gap-2" type="submit">
-                        <a id="button5" class="btn btn-secondary">
-                            Chỉnh sửa
-                        </a>
-                    </div>
+                <td colspan="4" >
+                    <button style="width: 100%;" type="button" class="btn btn-outline-dark" onclick="window.location.href='/admin/user-management.php';">Trở lại trang quản lý</button>
                 </td>
             </tr>
         </tbody>
