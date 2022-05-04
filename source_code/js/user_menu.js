6af9e0cfe279ffe4fb82cf34ba24d68cb8a378f9
nhankahi1254 <91319607+nhankahi1254@users.noreply.github.com>
2022-05-04 07:49:47 +0700

    Add files via upload

--- source_code/js/user_menu.js
+++ source_code/js/user_menu.js
@@ -1,33 +1,48 @@
+let preChangeName = document.getElementById("preChangeName");
+let postChangeName = document.getElementById("postChangeName");
+let toggleOnNameBut = document.getElementById("toggleChangeName");
+let toggleOffNameBut = document.getElementById("toggleChangeNameOff");
 
-$("#toggleChangeName").click(function(){
-    $("#preChangeName").css("display", "none");
-    $("#postChangeName").css("display", "inline");
-})
-$("#toggleChangeNameOff").click(function() {
-    $("#preChangeName").css("display", "inline");
-    $("#postChangeName").css("display", "none");
-})
-
-
+let preChangeEmail = document.getElementById("preChangeEmail");
+let postChangeEmail = document.getElementById("postChangeEmail");
+let toggleOnEmailBut = document.getElementById("toggleChangeEmail");
+let toggleOffEmailBut = document.getElementById("toggleChangeEmailOff");
 
 
-$("#toggleChangeEmail").click(function() {
-    $("#preChangeEmail").css("display", "none");
-    $("#postChangeEmail").css("display", "inline");
-})
-$("#toggleChangeEmailOff").click(function() {
-    $("#preChangeEmail").css("display", "inline");
-    $("#postChangeEmail").css("display", "none");
-})
+let preChangePass = document.getElementById("preChangePass");
+let postChangePass = document.getElementById("postChangePass");
+let toggleOnPassBut = document.getElementById("toggleChangePass");
+let toggleOffPassBut = document.getElementById("toggleChangePassOff");
 
 
-$("#toggleChangePass").click(function() {
-    $("#preChangePass").css("display", "none");
-    $("#postChangePass").css("display", "inline");
-})
+toggleOnNameBut.onclick = function() {
+    preChangeName.style.display = "none";
+    postChangeName.style.display = "inline";
+}
+toggleOffNameBut.onclick = function() {
+    preChangeName.style.display = "inline";
+    postChangeName.style.display = "none";
+}
 
-$("#toggleChangePassOff").click(function() {
-   $("#preChangePass").css("display", "inline");
-   $("#postChangePass").css("display", "none");
-})
+
+
+
+toggleOnEmailBut.onclick = function() {
+    preChangeEmail.style.display = "none";
+    postChangeEmail.style.display = "inline";
+}
+toggleOffEmailBut.onclick = function() {
+    preChangeEmail.style.display = "inline";
+    postChangeEmail.style.display = "none";
+}
+
+
+toggleOnPassBut.onclick = function() {
+    preChangePass.style.display = "none";
+    postChangePass.style.display = "inline";
+}
+toggleOffPassBut.onclick = function() {
+    preChangePass.style.display = "inline";
+    postChangePass.style.display = "none";
+}
 
